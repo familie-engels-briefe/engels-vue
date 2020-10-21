5921aad73454f19af18e150f1ba81d7447b8da4e
Tim Helfensdörfer <tim.helfensdoerfer@uni-wuppertal.de>
2020-10-21 10:18:02 +0200

    fix: manually add cors header in wordpress

--- wordpress/wp-content/themes/engels/functions.php
+++ wordpress/wp-content/themes/engels/functions.php
@@ -13,3 +13,12 @@ add_action( 'init', 'engels_navigation' );
  * Register rest routes
  */
 require_once dirname( __FILE__ ) . '/rest/register.php';
+
+/**
+ * Add CORS header
+ */
+function add_cors_http_header() {
+	header( "Access-Control-Allow-Origin: *" );
+}
+
+add_action( 'init', 'add_cors_http_header' );
