4f1e958b8d356a1f91c3928a3173e2d2ed457a10
Tim Helfensdörfer <tim.helfensdoerfer@uni-wuppertal.de>
2020-11-27 23:11:24 +0100

    feat: responsive wordpress columns
    closes #24

--- frontend/src/components/wordpress/BasePage.vue
+++ frontend/src/components/wordpress/BasePage.vue
@@ -18,5 +18,21 @@ export default {
 </script>
 
 <style scoped>
+/deep/ h1 {
+    @apply text-gold text-6xl font-bold leading-tight font-serif mb-8;
+}
 
+/deep/ .wp-block-columns {
+    @apply flex flex-row flex-wrap -mx-4;
+}
+
+/deep/ .wp-block-columns .wp-block-column {
+    @apply w-full px-4 mb-4;
+}
+
+@media (min-width: 1024px) {
+    /deep/ .wp-block-columns .wp-block-column {
+        @apply w-auto flex-1 px-4 mb-0;
+    }
+}
 </style>
