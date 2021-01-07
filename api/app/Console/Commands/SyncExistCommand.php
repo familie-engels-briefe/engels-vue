8a2810084e584a7c1e28699609d857452fa706b7
Tim Helfensdörfer <thelfensdrfer@gmail.com>
2021-01-07 13:19:12 +0100

    docs: fixed php style

--- api/app/Console/Commands/SyncExistCommand.php
+++ api/app/Console/Commands/SyncExistCommand.php
@@ -82,7 +82,7 @@ class SyncExistCommand extends Command
         foreach ($index->letter as $letter) {
             $filename = (string) $letter['filename'];
 
-            $document = $this->replication->createRequest('data/'.$filename,
+            $document = $this->replication->createRequest('data/' . $filename,
                 'GET', [], false);
 
             // Sync facsimile
@@ -90,7 +90,7 @@ class SyncExistCommand extends Command
                 $xml = simplexml_load_string($document['body']);
                 $this->syncFacsimile($xml);
             } catch (Exception $e) {
-                $error = 'Could not parse xml from letter: '.$filename;
+                $error = 'Could not parse xml from letter: ' . $filename;
                 $this->error($error);
                 $this->comment('');
                 Log::error($error);
@@ -102,7 +102,7 @@ class SyncExistCommand extends Command
             $this->syncHtml('norm', (string) $letter['n']);
 
             // Update xml
-            if ( ! $this->option('dry-run')) {
+            if (!$this->option('dry-run')) {
                 $this->exist->updateDocument($filename, $document['body']);
             }
 
@@ -130,7 +130,7 @@ class SyncExistCommand extends Command
             $replicationLetters);
 
         foreach ($lettersToDelete as $letterToDelete) {
-            if ( ! $this->option('dry-run')) {
+            if (!$this->option('dry-run')) {
                 $this->exist->deleteDocument($letterToDelete);
             }
 
@@ -151,48 +151,50 @@ class SyncExistCommand extends Command
     }
 
     /**
-     * @param  string  $type
-     * @param  string  $number
+     * @param string $type
+     * @param string $number
      */
     private function syncHtml(string $type, string $number): void
     {
-        if ( ! in_array($type, ['dipl', 'norm'])) {
+        if (!in_array($type, ['dipl', 'norm'])) {
             throw new InvalidArgumentException('Only "dipl" and "norm" html types are allowed!');
         }
 
-        $url = 'api/'.config('app.exist_api_version').'/letter/fe.'.$number
-               .'/html/'.$type;
+        $url =
+            'api/' . config('app.exist_api_version') . '/letter/fe.' . $number
+            . '/html/' . $type;
         $response = $this->replication->createRequest($url, 'GET', [], false);
-        if ( ! isset($response['body'])) {
-            $this->error('Could not read body from '.$type.' letter html: '
-                         .$url);
+        if (!isset($response['body'])) {
+            $this->error('Could not read body from ' . $type . ' letter html: '
+                         . $url);
             $this->comment('');
 
             Log::error('Could not read body from response: '
-                       .serialize($response));
-        } elseif ( ! $this->option('dry-run')) {
-            $key = $number.'-html-'.$type;
+                       . serialize($response));
+        } elseif (!$this->option('dry-run')) {
+            $key = $number . '-html-' . $type;
             Cache::tags([$type, 'html', $number])->put($key, $response['body']);
         }
     }
 
     /**
-     * @param  SimpleXMLElement  $xml
+     * @param SimpleXMLElement $xml
      */
     private function syncFacsimile(SimpleXMLElement $xml): void
     {
         if (!isset($xml->facsimile)) {
             Log::error(sprintf('No facsimile for xml: %s', serialize($xml)));
+
             return;
         }
 
-        if ( ! $this->option('dry-run')) {
+        if (!$this->option('dry-run')) {
             foreach ($xml->facsimile->graphic as $fac) {
                 $url = (string) $fac['url'];
                 $pdf = $this->replication->createRequest(sprintf('facs/%s',
                     $url), 'GET', [], false);
 
-                if ( ! isset($pdf['body'])) {
+                if (!isset($pdf['body'])) {
                     Log::error(sprintf('Facsimile does not exist: %s', $url));
                     continue;
                 }
@@ -230,7 +232,7 @@ class SyncExistCommand extends Command
                 $this->replication->createRequest(sprintf('data/register/reg_global_%s.xml',
                     $register), 'GET', [], false);
 
-            if ( ! $this->option('dry-run')) {
+            if (!$this->option('dry-run')) {
                 $this->exist->updateDocument(sprintf('register/reg_global_%s.xml',
                     $register), $document['body']);
             }
