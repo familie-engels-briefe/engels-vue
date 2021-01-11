f2f77014eed0370ef95523fa425b63432f93108c
Tim Helfensdörfer <thelfensdrfer@gmail.com>
2021-01-11 12:06:28 +0100

    feat: added fixed facsimile
    closes #58

--- frontend/src/components/letter/detail/Facsimile.vue
+++ frontend/src/components/letter/detail/Facsimile.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <div class="pdf-viewer" v-text="error"></div>
+        <div class="pdf-viewer"
+             v-text="error"></div>
     </div>
 </template>
 
@@ -23,13 +24,26 @@ export default {
     },
     data () {
         return {
-            error: ''
+            error: '',
+            offsetTop: 0
         }
     },
     mounted () {
         console.debug('Mounted Letter/Facsimile')
 
         this.displayPdf()
+
+        const pdfViewer = this.$el.querySelector('.pdf-viewer')
+        this.offsetTop = pdfViewer.getBoundingClientRect().top
+
+        document.addEventListener('scroll', this.scrollHandler)
+        this.scrollHandler()
+
+        window.addEventListener('resize', this.resizeHandler)
+        this.resizeHandler()
+    },
+    beforeDestroy () {
+        document.removeEventListener('scroll', this.scrollHandler)
     },
     methods: {
         displayPdf () {
@@ -39,6 +53,47 @@ export default {
                 })
             } else {
                 this.error = 'Es gibt zu diesem Brief keine Facsimiles.'
+            }
+        },
+        scrollHandler () {
+            const pdfViewer = this.$el.querySelector('.pdf-viewer')
+            const scrollTop = window.pageYOffset
+            const browserWidth = Math.max(
+                document.body.scrollWidth,
+                document.documentElement.scrollWidth,
+                document.body.offsetWidth,
+                document.documentElement.offsetWidth,
+                document.documentElement.clientWidth
+            )
+
+            // Add/remove fixed position of facsimile based on scrolling position
+            if (browserWidth >= 1024 && scrollTop >= this.offsetTop && !pdfViewer.classList.contains('fixed')) {
+                pdfViewer.classList.add('fixed', 'top-0')
+            } else if (browserWidth < 1024 || (scrollTop < this.offsetTop && pdfViewer.classList.contains('fixed'))) {
+                pdfViewer.classList.remove('fixed', 'top-0')
+            }
+        },
+        resizeHandler () {
+            const pdfViewer = this.$el.querySelector('.pdf-viewer')
+
+            // Set pdf width to parent node width.
+            // If the position is fixed, it is not calculated correctly
+            pdfViewer.style.width = pdfViewer.parentNode.offsetWidth + 'px'
+
+            const browserWidth = Math.max(
+                document.body.scrollWidth,
+                document.documentElement.scrollWidth,
+                document.body.offsetWidth,
+                document.documentElement.offsetWidth,
+                document.documentElement.clientWidth
+            )
+
+            // Check if pdf should be fixed if browser gets wider
+            this.scrollHandler()
+
+            // Remove the fixed position if the browser gets too small
+            if (browserWidth < 1024 && pdfViewer.classList.contains('fixed')) {
+                pdfViewer.classList.remove('fixed', 'top-0')
             }
         }
     },
