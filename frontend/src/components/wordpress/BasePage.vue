943f68c28ac9e38951928a4e0afa5ab01801a7e0
Tim Helfensdörfer <thelfensdrfer@gmail.com>
2021-01-17 23:05:35 +0100

    feat: optimized wordpress links and columns

--- frontend/src/components/wordpress/BasePage.vue
+++ frontend/src/components/wordpress/BasePage.vue
@@ -22,21 +22,25 @@ export default {
     @apply text-gold text-6xl font-bold leading-tight font-serif mb-8;
 }
 
-/deep/ .wp-block-columns {
-    @apply flex flex-row flex-wrap -mx-4;
-}
-
-/deep/ .wp-block-columns .wp-block-column {
-    @apply w-full px-4 mb-4;
-}
-
 /deep/ img {
     @apply block w-full;
 }
 
+/deep/ a {
+    @apply underline;
+}
+
+/deep/ .wp-block-columns {
+    @apply flex flex-row flex-wrap -mx-8;
+}
+
+/deep/ .wp-block-columns .wp-block-column {
+    @apply w-full px-8 mb-4;
+}
+
 @media (min-width: 1024px) {
     /deep/ .wp-block-columns .wp-block-column {
-        @apply w-auto flex-1 px-4 mb-0;
+        @apply w-auto flex-1 mb-0;
     }
 }
 </style>
