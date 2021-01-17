50521e37caa98f7a038717aa7b7fe9692d7312ae
Tim Helfensdörfer <thelfensdrfer@gmail.com>
2021-01-17 23:08:45 +0100

    feat: copyright is not necessary

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="bg-gray-darker text-gray-lightest text-sm">
         <div class="max-w-6xl mx-auto px-8 md:px-4 lg:px-0 py-4 leading-none flex justify-between">
             <div>
-                &copy; 2020 | <router-link to="/datenschutzerklärung">Datenschutzerklärung</router-link> | <router-link to="/impressum">Impressum</router-link>
+                <router-link to="/datenschutzerklärung">Datenschutzerklärung</router-link> | <router-link to="/impressum">Impressum</router-link>
             </div>
             <div>
                 <a href="https://github.com/familie-engels-briefe/webapp/"><font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon></a>
