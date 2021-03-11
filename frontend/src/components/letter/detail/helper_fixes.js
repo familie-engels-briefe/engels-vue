07a8e7cebefda824b748c6e7c89e6de85ecfe3ce
Tim Helfensdörfer <thelfensdrfer@gmail.com>
2021-03-11 16:31:23 +0100

    feat: changed einweisungszeichen

--- frontend/src/components/letter/detail/helper_fixes.js
+++ frontend/src/components/letter/detail/helper_fixes.js
@@ -5,4 +5,8 @@ export function replaceSymbols (wrapper) {
     wrapper.querySelectorAll('.tei_metamark[data-function="separator"]').forEach(function (separator) {
         separator.innerHTML = '|'
     })
+
+    wrapper.querySelectorAll('[data-rendition="#g.enc.tagsdecl.metamark.insertion"]').forEach(function (rendition) {
+        rendition.innerHTML = '&int;'
+    })
 }
